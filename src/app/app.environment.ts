98e57c241ec0839e35be33d5d7ff1284d4003e58
Gavin Mitchell <gavinm@ulogic.co.uk>
2018-03-18 16:14:06 +0000

    build

--- src/app/app.environment.ts
+++ src/app/app.environment.ts
@@ -1,5 +1,5 @@
 export const Environment = {
     production: false,
     // API_BASE: 'http://localhost:52414/api/'
-    API_BASE: 'http://192.168.0.35/homecontrol/server/api/'
+    API_BASE: 'http://elephant/homecontrol/server/api/'
 };
