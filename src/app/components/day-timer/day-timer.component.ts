1dfe00bba83c116a6f9499766704d9816731323d
Gavin Mitchell <gavinm@ulogic.co.uk>
2020-11-10 22:57:19 +0000

    working except for event drag

--- src/app/components/day-timer/day-timer.component.ts
+++ src/app/components/day-timer/day-timer.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, Output, EventEmitter, ElementRef, OnInit, OnChanges } from '@angular/core';
 import { LoggerService } from '../../services/logger.service';
 import { HeatingService } from '../../services/heating.service';
-import { TimedEvent } from '../../models/timed-event';
+import { TimedEvent, TimedEventPeriod } from '../../models/timed-event';
 import { DatePipe } from '@angular/common';
 
 @Component({
@@ -16,9 +16,9 @@ export class DayTimerComponent implements OnInit {
     @Input() selectedEvent: TimedEvent;
     @Output() onEventSelected = new EventEmitter<TimedEvent>();
 
-    public dragPeriod: any = null;
-    public dayStart: Date;
-    public dayEnd: Date;
+    // public dragPeriod: any = null;
+    // public dayStart: Date;
+    // public dayEnd: Date;
 
     public hours: number[] = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 0 ];
     public ctrlWidth = 320;
@@ -26,13 +26,11 @@ export class DayTimerComponent implements OnInit {
     public barLeft = 10;
 
     public filteredEvents: TimedEvent[];
-    // public selectedPeriod: any = null;
+    public selectedPeriod: TimedEvent = null;
     public dayArray: any[];
     public repeating: boolean;
     public showCommit: boolean = false;
     public dayLabel: string = '';
-
-    public selectedPeriod: TimedEvent;
 
     constructor(
         private elementRef: ElementRef,
@@ -42,11 +40,11 @@ export class DayTimerComponent implements OnInit {
 
     private init() {
         this.handleResize(null);
-console.log('filtering ' + this.events.length + ' events, with filter: ' + this.dayFilter)
-        this.filteredEvents = this.events.filter(e => (e.timeStart.getFullYear() < 1000 && (e.timeStart.getFullYear() & this.dayFilter) > 0) 
-            || (this.dayFilter > 127 && e.timeStart.getFullYear() > 1000));
+        console.log('filtering ' + this.events.length + ' events, with filter: ' + this.dayFilter)
+        this.filteredEvents = this.events.filter(e => (!e.periodObj.absoluteDate && (e.periodObj.days & this.dayFilter) > 0) 
+            || (this.dayFilter > 127 && e.periodObj.absoluteDate));
       
-        this.repeating = this.dayFilter > 0 && this.dayFilter < 1000;
+        this.repeating = this.dayFilter > 0 && this.dayFilter < 128;
         /* tslint:disable:no-bitwise */
         this.dayArray = [{name: 'Mon', selected: this.dayFilter & 1},
                                 {name: 'Tue', selected: this.repeating && this.dayFilter & 2},
@@ -103,126 +101,120 @@ console.log('filtering ' + this.events.length + ' events, with filter: ' + this.
       }
     }
     
-    public setDragPeriod(period: any) {
-        this.dragPeriod = {};
-        this.dragPeriod.timeStart = new Date(period.timeStart);
-        this.dragPeriod.timeEnd = new Date(period.timeEnd);
+    public setDragPeriod(timedEvent: TimedEvent) {
+        // this.dragPeriod = {};
+        // this.dragPeriod.timeStart = new Date(period.timeStart);
+        // this.dragPeriod.timeEnd = new Date(period.timeEnd);
 
-        this.dayStart = new Date(period.timeStart);
-        this.dayStart.setHours(0, 0, 0, 0);
-        this.dayEnd = new Date(this.dayStart);
-        this.dayEnd.setHours(23, 59, 59, 999);
+        // this.dayStart = new Date(period.timeStart);
+        // this.dayStart.setHours(0, 0, 0, 0);
+        // this.dayEnd = new Date(this.dayStart);
+        // this.dayEnd.setHours(23, 59, 59, 999);
     };
 
     public convertPixelToMilliseconds(value: number) {
         return value * 3600000 / ((this.ctrlWidth - 20) / 24);
     };
 
+    public convertPixelToSeconds(value: number) {
+        return value * 3600 / ((this.ctrlWidth - 20) / 24);
+    };
+
     public isValidPeriod(timedEvent: TimedEvent) {
-        if (!timedEvent || !timedEvent.timeStart || !this.filteredEvents.find(e => e === timedEvent)) { return false; }
+        if (!timedEvent || !timedEvent.periodObj || !this.filteredEvents.find(e => e === timedEvent)) { return false; }
         // let start: Date = new Date(period.timeStart);
         return true;
     };
 
-    public getPeriodStartX(period: any) {
-        let start: Date = new Date(period.timeStart);
-        return (this.ctrlWidth - 20) / 24 * (start.getHours() + start.getMinutes() / 60) + 10;
+    public getPeriodStartX(timedEvent: TimedEvent) {
+        return (this.ctrlWidth - 20) * timedEvent.periodObj.time / 86400 + 10;  // 86400 sec per day
     };
 
-    public getPeriodEndX(period: any) {
-        let end: Date = new Date(period.timeEnd);
-        return (this.ctrlWidth - 20) / 24 * (end.getHours() + end.getMinutes() / 60) + 10;
+    public getPeriodEndX(timedEvent: TimedEvent) {
+        return (this.ctrlWidth - 20) * (timedEvent.periodObj.time + timedEvent.periodObj.duration) / 86400 + 10;
     };
 
-    public getPeriodWidth(period: any) {
-        let start: Date = new Date(period.timeStart);
-        let end: Date = new Date(period.timeEnd);
-        let p: number = end.getTime() - start.getTime();
-        return (p / 3600000) * (this.ctrlWidth - 20) / 24;
+    public getPeriodWidth(timedEvent: TimedEvent) {
+        return (timedEvent.periodObj.duration / 86400) * (this.ctrlWidth - 20);
     };
 
-    public onTapPeriod($event: any, period: any) {
+    public onTapPeriod($event: any, timedEvent: TimedEvent) {
       LoggerService.log('onTapPeriod');
-        if (this.selectedPeriod === period) {
+        if (this.selectedPeriod === timedEvent) {
             this.selectedPeriod = null;
             this.onEventSelected.emit(this.selectedPeriod);
-            this.commitPeriodUpdate(period);
+            this.commitPeriodUpdate(timedEvent);
         } else {
-            this.selectedPeriod = period;
+            this.selectedPeriod = timedEvent;
             this.onEventSelected.emit(this.selectedPeriod);
         }
     };
 
-    private periodDeleteIfEmpty(period: any) {
-        let self = this;
-        let testStart = new Date(period.timeStart);
-        let testEnd = new Date(period.timeEnd);
-        if (testStart.getTime() === testEnd.getTime()) {
-            if (period.id > 0) {
-                self.heatingService.deleteEvent(period)
-                    .subscribe( (res: any) => {
-                        period = {};
-                        self.selectedPeriod = null;
-                    });
-            } else {
-                period = {};
-                this.selectedPeriod = null;
-            }
-            LoggerService.log('DayTimer.periodDeleteIfEmpty period deleted');
-            this.onEventSelected.emit(null);
-            return true;
-        }
+    private periodDeleteIfEmpty(period: any): boolean {
+        // let self = this;
+        // let testStart = new Date(period.timeStart);
+        // let testEnd = new Date(period.timeEnd);
+        // if (testStart.getTime() === testEnd.getTime()) {
+        //     if (period.id > 0) {
+        //         self.heatingService.deleteEvent(period)
+        //             .subscribe( (res: any) => {
+        //                 period = {};
+        //                 self.selectedPeriod = null;
+        //             });
+        //     } else {
+        //         period = {};
+        //         this.selectedPeriod = null;
+        //     }
+        //     LoggerService.log('DayTimer.periodDeleteIfEmpty period deleted');
+        //     this.onEventSelected.emit(null);
+        //     return true;
+        // }
         return false;
     };
 
-    public onDragPeriod($event: any, period: any) {
+    public onDragPeriod($event: any, timedEvent: TimedEvent) {
         $event.preventDefault();        
         switch ($event.type) {
             case 'panstart':
-                this.setDragPeriod(period);
+                this.setDragPeriod(timedEvent);
                 break;
             case 'pan':
-                period.timeStart = new Date(); period.timeStart.setTime(this.dragPeriod.timeStart.getTime() + this.convertPixelToMilliseconds($event.deltaX));
-                period.timeEnd = new Date(); period.timeEnd.setTime(this.dragPeriod.timeEnd.getTime() + this.convertPixelToMilliseconds($event.deltaX));
+                timedEvent.setTime(this.convertPixelToSeconds($event.deltaX));
                 break;
             case 'panend':
-                this.dragPeriod = null;
+                // this.dragPeriod = null;
                 break;
         }
     };
 
-    public onDragTimeStart($event: any, period: any) {
+    public onDragTimeStart($event: any, timedEvent: TimedEvent) {
         $event.preventDefault();        
         switch ($event.type) {
             case 'panstart':
-                this.setDragPeriod(period);
+                this.setDragPeriod(timedEvent);
                 break;
             case 'pan':
-                period.timeStart = new Date(); period.timeStart.setTime(this.dragPeriod.timeStart.getTime() + this.convertPixelToMilliseconds($event.deltaX));
-                if (period.timeStart.getTime() > this.dragPeriod.timeEnd.getTime()) { period.timeStart.setTime(this.dragPeriod.timeEnd.getTime()); }
-                if (period.timeStart.getTime() < this.dayStart.getTime()) { period.timeStart.setTime(this.dayStart.getTime()); }
+                timedEvent.shiftTime(this.convertPixelToSeconds($event.deltaX));
                 break;
             case 'panend':
-                this.dragPeriod = null;
-                if (this.periodDeleteIfEmpty(period)) { this.init(); }
+                // this.dragPeriod = null;
+                if (this.periodDeleteIfEmpty(timedEvent)) { this.init(); }
                 break;
         }
     };
 
-    public onDragTimeEnd($event: any, period: any) {
+    public onDragTimeEnd($event: any, timedEvent: TimedEvent) {
         $event.preventDefault();        
         switch ($event.type) {
             case 'panstart':
-                this.setDragPeriod(period);
+                this.setDragPeriod(timedEvent);
                 break;
             case 'pan':
-                period.timeEnd = new Date(); period.timeEnd.setTime(this.dragPeriod.timeEnd.getTime() + this.convertPixelToMilliseconds($event.deltaX));
-                if (period.timeEnd.getTime() < this.dragPeriod.timeStart.getTime()) { period.timeEnd.setTime(this.dragPeriod.timeStart.getTime()); }
-                if (period.timeEnd.getTime() > this.dayEnd.getTime()) { period.timeEnd.setTime(this.dayEnd.getTime()); }
+                timedEvent.shiftDuration(this.convertPixelToSeconds($event.deltaX));
                 break;
             case 'panend':
-                this.dragPeriod = null;
-                if (this.periodDeleteIfEmpty(period)) { this.init(); }
+                // this.dragPeriod = null;
+                if (this.periodDeleteIfEmpty(timedEvent)) { this.init(); }
                 break;
         }
     };
--- src/app/models/timed-event.ts
+++ src/app/models/timed-event.ts
@@ -1,13 +1,130 @@
+export class TimedEventPeriod {
+    code: string;
+    days: number;
+    absoluteDate: Date;
+    time: number;
+    duration: number;
+
+    constructor(code: string) {
+        this.code = code;
+        
+        let codeSplit = code.split('-');
+        if (codeSplit.length != 3) { return; }
+
+        let days = parseInt(codeSplit[0]);
+
+        if (days > 127) {
+            this.absoluteDate = new Date(codeSplit[0])
+        } else {
+            this.days = days;
+        }
+
+        this.time = parseInt(codeSplit[1]);
+        this.duration = parseInt(codeSplit[2]);
+    }
+
+    private formatDate(date) {
+        var d = new Date(date),
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate(),
+            year = d.getFullYear();
+    
+        if (month.length < 2) 
+            month = '0' + month;
+        if (day.length < 2) 
+            day = '0' + day;
+    
+        return [year, month, day].join('-');
+    }
+
+    private refreshCode() {
+        this.code = `${(this.absoluteDate ? this.formatDate(this.absoluteDate) : this.days)}-${this.time}-${this.duration}`;
+    }
+
+    // public setTime(timeSecs: number) {
+    //     this.time = timeSecs;
+    //     this.refreshCode();
+    // }
+
+    public setDays(days: number): void {
+        this.days = days;
+        this.refreshCode();
+    }
+
+    public setTime(time: number): void {
+        this.time = time;
+        this.refreshCode();
+    }
+
+    public shiftTime(timeSecs: number): void {
+        if (timeSecs >= this.time + this.duration) {
+            this.time = this.time + this.duration;
+            this.duration = 0;
+        } else {
+            this.time = timeSecs;
+            this.duration = this.duration + this.time - timeSecs;
+        }
+        this.refreshCode();
+    }
+
+    public shiftDuration(timeSecs: number): void {
+        if (timeSecs <= this.time) {
+            this.duration = 0;
+        } else {
+            this.duration = timeSecs - this.time;
+        }
+        this.refreshCode();
+    }
+}
+
 export class TimedEvent {
     description: string;
     id: number;
     isGroup: boolean;
     subjectId: number;
     subjectType: string;
-    timeEnd: Date;
-    timeEndStr: string;
-    timeStart: Date;
-    timeStartStr: string;
+    period: string;
+    periodObj: TimedEventPeriod;
     action: string;
-}
 
+    // public setTime(timeSecs: number) {
+    //     this.periodObj.setTime(timeSecs);
+    //     this.period = this.periodObj.code;
+    // }
+
+    public static mapMany(sourceList: TimedEvent[]): TimedEvent[] {
+        const retList: TimedEvent[] = [];
+        sourceList.forEach( x => {
+            retList.push(this.map(x));
+        });
+        return retList;
+    }
+
+    public static map(source: TimedEvent): TimedEvent {
+        const newEvent = new TimedEvent;
+        for (let key of Object.keys(source)) {
+            newEvent[key] = source[key];
+        }
+        return newEvent;
+    }
+
+    public setDays(days: number): void {
+        this.periodObj.setDays(days);
+        this.period = this.periodObj.code;
+    }
+
+    public setTime(time: number): void {
+        this.periodObj.setTime(time);
+        this.period = this.periodObj.code;
+    }
+
+    public shiftTime(timeSecs: number): void {
+        this.periodObj.shiftTime(timeSecs);
+        this.period = this.periodObj.code;
+    }
+
+    public shiftDuration(timeSecs: number): void {
+        this.periodObj.shiftDuration(timeSecs);
+        this.period = this.periodObj.code;
+    }
+}
